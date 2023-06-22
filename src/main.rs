21a4aa9e8192b57db314d26e5b5cc8088f384e91
ihab65 <ihg.bk65@gmail.com>
2023-06-22 13:54:24 +0100

    added more TUI elements

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use rodio::{Decoder, OutputStream, Sink};
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHTED_PAIR: i16 = 1;
+const COLORED_PAIR: i16 = 2;
 
 enum Status {
     Paused,
@@ -43,7 +44,7 @@ fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     
-
+    
     initscr();
     start_color();
     noecho();
@@ -51,24 +52,43 @@ fn main() {
 
     init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
     init_pair(HIGHLIGHTED_PAIR, COLOR_BLACK, COLOR_WHITE);
+    init_pair(COLORED_PAIR, COLOR_GREEN, COLOR_YELLOW);
 
     let mut quit = false;
     let mut ui = Ui::default();
     let mut status = Status::Paused;
 
+    let path = "test.mp3";
+
     while !quit {
         erase();
         ui.begin(0, 0);
         match status {
-            Status::Paused => ui.label("[pause] play ", REGULAR_PAIR),
-            Status::Playing=> ui.label(" pause [play]", REGULAR_PAIR)
+            Status::Paused => {
+                ui.label("[pause] play ", REGULAR_PAIR);
+                ui.label(&format!("the current track `{}` is paused .", path), REGULAR_PAIR)
+            },
+            Status::Playing=> {
+                ui.label(" pause [play]", REGULAR_PAIR);
+                ui.label(&format!("the current track `{}` is playing .", path), REGULAR_PAIR)
+            }
         }
+        
+        ui.label(" press Enter to launch ", HIGHLIGHTED_PAIR);
+        mv(2, 24);
+        attron(COLOR_PAIR(HIGHLIGHTED_PAIR)); 
+        addstr(" press q to quit ");
+        attroff(COLOR_PAIR(HIGHLIGHTED_PAIR));
+        mv(2, 42);
+        attron(COLOR_PAIR(COLORED_PAIR)); 
+        addstr(" press Space to pause/play ");
+        attroff(COLOR_PAIR(COLORED_PAIR));
 
         let key = getch();
 
         match key as u8 as char {
             '\n' => {
-                let file = BufReader::new(File::open("test.mp3")
+                let file = BufReader::new(File::open(path)
                     .expect("ERROR: No such file or directory"));
                 let source = Decoder::new(file).unwrap();
                 sink.append(source); 
