a0eb59c8e3b35d699b7bc8a443f142ca4b492513
ihab65 <ihg.bk65@gmail.com>
2023-06-23 20:19:25 +0100

    added the ability to parse the 1st argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use ncurses::*;
-use std::fs::File;
+use std::env;
+use std::{fs::File, process};
 use std::io::BufReader;
 use rodio::{Decoder, OutputStream, Sink};
+
+// use walkdir::DirEntry;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHTED_PAIR: i16 = 1;
@@ -38,9 +41,54 @@ impl Ui {
         self.row += 1;
     }
 }
+// Logic for reading Dir's
+// --- --- --- --- --- --- --- --- --- --- --- ---
+// fn get_path(paths_lsit: Vec<String>) {
+//     todo!()
+// }
+
+// fn get_file(entry: DirEntry) -> Result<String, ()> {
+//     match entry.path().to_str() {
+//         Some(file) => {
+//             let file = file.to_string();
+//             Ok(file)
+//         },
+//         None => {
+//             Err(println!("ERROR: failed to convert `DirEntry` to var of type String"))
+//         }
+//     }
+// }
 
 fn main() {
     
+    let mut args = env::args();
+    args.next().unwrap();
+
+    let file_path = match args.next() {
+        Some(file_path) => file_path,
+        None => {
+            eprintln!("Usage: mp-rs <file-path>");
+            eprintln!("ERROR: Dir or file path is not provided");
+            process::exit(1);
+        }
+    };
+
+    // Logic for reading Dir's
+    // --- --- --- --- --- --- --- --- --- --- --- ---
+    // let mut songs = Vec::<String>::new();
+
+    // for entry in WalkDir::new(file_path.clone()) {
+    //     match entry {
+    //         Ok(entry) => {
+    //             songs.push(
+    //                 get_file(entry).unwrap()
+    //             );
+    //         },
+    //         Err(_) => eprintln!("ERROR: specifed path doesn't exist")
+    //     }
+    // }
+    // process::exit(1);
+
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     
@@ -57,40 +105,43 @@ fn main() {
     let mut quit = false;
     let mut ui = Ui::default();
     let mut status = Status::Paused;
-
-    let path = "test.mp3";
+    let path = file_path;
 
     while !quit {
         erase();
-        ui.begin(0, 0);
-        match status {
-            Status::Paused => {
-                ui.label("[pause] play ", REGULAR_PAIR);
-                ui.label(&format!("the current track `{}` is paused .", path), REGULAR_PAIR)
-            },
-            Status::Playing=> {
-                ui.label(" pause [play]", REGULAR_PAIR);
-                ui.label(&format!("the current track `{}` is playing .", path), REGULAR_PAIR)
+        // Ui Block
+        { 
+            ui.begin(0, 0);
+            match status {
+                Status::Paused => {
+                    ui.label("[pause] play ", REGULAR_PAIR);
+                    ui.label(&format!("the current track `{:?}` is paused .", path), REGULAR_PAIR)
+                },
+                Status::Playing=> {
+                    ui.label(" pause [play]", REGULAR_PAIR);
+                    ui.label(&format!("the current track `{:?}` is playing .", path), REGULAR_PAIR)
+                }
+
             }
+            ui.label(" press Enter to launch ", HIGHLIGHTED_PAIR);
+            mv(2, 24);
+            attron(COLOR_PAIR(HIGHLIGHTED_PAIR)); 
+            addstr(" press q to quit ");
+            attroff(COLOR_PAIR(HIGHLIGHTED_PAIR));
+            mv(2, 42);
+            attron(COLOR_PAIR(COLORED_PAIR)); 
+            addstr(" press Space to pause/play ");
+            attroff(COLOR_PAIR(COLORED_PAIR));
         }
-        
-        ui.label(" press Enter to launch ", HIGHLIGHTED_PAIR);
-        mv(2, 24);
-        attron(COLOR_PAIR(HIGHLIGHTED_PAIR)); 
-        addstr(" press q to quit ");
-        attroff(COLOR_PAIR(HIGHLIGHTED_PAIR));
-        mv(2, 42);
-        attron(COLOR_PAIR(COLORED_PAIR)); 
-        addstr(" press Space to pause/play ");
-        attroff(COLOR_PAIR(COLORED_PAIR));
 
         let key = getch();
 
         match key as u8 as char {
             '\n' => {
-                let file = BufReader::new(File::open(path)
+                let file = BufReader::new(File::open(path.clone())
                     .expect("ERROR: No such file or directory"));
                 let source = Decoder::new(file).unwrap();
+                    
                 sink.append(source); 
                 status = Status::Playing;
             },
